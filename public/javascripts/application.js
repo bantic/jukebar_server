77fc202a8563f018cd3ca2e41a7eb915e05174da
Cory Forsyth <cory.forsyth@gmail.com>
2011-03-22 22:23:33 -0400

    stop using data-votes because jquery doesn't update it when we resort

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,13 @@ function sortBarSongsByVotes() {
   bar_songs_list = $("#bar_songs_list");
   bar_songs = bar_songs_list.children("li").get();
   sorted_bar_songs = bar_songs.sort(function(a,b) {
-    var votesA = parseInt($(a).data("votes"), 10);
-    var votesB = parseInt($(b).data("votes"), 10);
+    var votesA = parseInt($(a).find(".actual_vote_count").html(), 10);
+    var votesB = parseInt($(b).find(".actual_vote_count").html(), 10);
     return (votesA < votesB) ? 1 : (votesA > votesB) ? -1 : 0;    
   });
   bar_songs_list.empty();
   $.each(sorted_bar_songs, function(idx, song) { 
-    console.log("idx: " + idx + ", song: " + song);
+    console.log("idx: " + idx + ", song: " + song + " votes: " + $(song).data("votes"));
     bar_songs_list.append(song); 
   });
 }
